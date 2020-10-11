2afa9f2a5fc2ca12964a67fef0295c18f52535cc
undstory <undrul.ozog@gmail.com>
2020-10-11 14:07:49 +0200

    navigation

---
+++ src/components/Navigation.vue
@@ -0,0 +1,85 @@
+<template>
+
+    <div class="nav">
+        <div class="container">
+            <div class="nav__main">
+                <div class="nav__logo"><router-link to="/" class="nav__logo">photo_my_love</router-link></div>
+                <nav class="nav__menu">
+                    <router-link to="/" class="nav__link">Home</router-link>
+                    <router-link to="/about" class="nav__link">About</router-link>
+                </nav>
+            </div>
+           
+           <button class="nav__contact"><router-link to="/contact" class="nav__contact--link">Contact</router-link></button>
+        
+        </div>
+     
+   
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Navi'
+}
+</script>
+
+<style lang="scss" scoped>
+
+@import "../styles/variables.scss";
+@import "../styles/mixins.scss";
+
+    .nav {
+        padding: 3rem 1.5rem 2.5rem;
+        background-color: $background-top;
+        color: $font-color;
+    }
+
+    .container {
+        @include container;
+        display: flex;
+        justify-content: space-between;
+        align-items: baseline;
+    }
+
+    .nav__main {
+        display: flex;
+        flex-direction: row;
+        align-items: flex-end;
+    }
+
+    .nav__link {
+        color: $font-color;
+        text-decoration: none;
+        padding: 0 2rem 0 1rem;
+    }
+
+    .nav__menu {
+        font-size: 1.4rem;
+        text-transform: lowercase;
+    }
+
+    .nav__logo {
+        color: $font-color;
+        text-decoration: none;
+        padding-right: 3rem;
+        // font-weight: bold;
+        font-size: 2.8rem;
+    }
+
+    .nav__contact {
+        border: 2px solid $font-color;
+        border-radius:  15px;
+        background-color: transparent;
+        padding: .8rem 1.5rem;
+
+        &--link {
+            text-decoration: none;
+            color: $font-color;
+            text-transform: uppercase;
+            font-size: 1rem;
+            letter-spacing: 1px;
+        }
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Contact from '../views/Contact.vue'
 
 Vue.use(VueRouter)
 
@@ -13,12 +15,12 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
+    component: About
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: Contact
   }
 ]
 
